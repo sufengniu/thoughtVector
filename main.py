488502e67ae88860da9577fbfc1c26fde2b2ac4c
Sufeng Niu <sniu@g.clemson.edu>
2016-04-26 22:21:44 -0400

    main.py is running but results incorrect, debugging

--- main.py
+++ main.py
@@ -17,25 +17,9 @@ import tensorflow as tf
 from tensorflow.contrib import skflow
 
 import data_utils
+from word_utils import TextLoader
 
-_PAD = "_PAD"
-_UNK = "_UNK"
-_POS = "_POS"
-_NEG = "_NEG"
-
-_START_VOCAB = [_PAD, _UNK, _POS, _NEG]
-
-PAD_ID = 0
-UNK_ID = 1
-POS_ID = 2
-NEG_ID = 3
-
-# Regular expressions used to tokenize.
-_WORD_SPLIT = re.compile("([.,!?\"':;)(])")
-_DIGIT_RE = re.compile(r"\d")
-_buckets = [(80, 1), (160, 1), (320, 1), (640, 1), (1280,1)]
-
-
+unsup_buckets = [(80, 80), (160, 160), (320, 320), (640, 640), (1280,1280)]
 
 # load data
 """
@@ -50,12 +34,12 @@ flags=tf.app.flags
 
 # RNN configuration
 flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
-flags.DEFINE_float("dropout", 1, "dropout rates")
+flags.DEFINE_float("dropout", 0.8, "dropout rates")
 flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm.")
-flags.DEFINE_integer("batch_size", 64, "Batch size to use during training.")
+flags.DEFINE_integer("batch_size", 4, "Batch size to use during training.")
 flags.DEFINE_integer("max_len", 100, "sequence length longer than this will be ignored.")
-flags.DEFINE_integer("embedding_size", 128, "Size of each model layer.")
-flags.DEFINE_integer("depth", 3, "Number of layers in the model.")
+flags.DEFINE_integer("embedding_size", 16, "Size of each model layer.")
+flags.DEFINE_integer("depth", 1, "Number of layers in the model.")
 flags.DEFINE_integer("vocab_size", 100000, "vocabulary size.")
 flags.DEFINE_string("data_dir", "data", "Data directory")
 flags.DEFINE_string("train_dir", "data", "Training directory.")
@@ -68,6 +52,8 @@ flags.DEFINE_boolean("validation", False, "Run validation if true")
 
 FLAGS = flags.FLAGS
 
+max_step = 1000000
+
 # testing
 def embedding_seq2seq(X, do_decode):
 	with variable_scope.variable_scop(scope or "embedding_seq2seq"):
@@ -79,28 +65,34 @@ def embedding_seq2seq(X, do_decode):
 		#if isinstance(do_decode, bool):
 
 class rnnAutoEncoder(object):
-	def __init__(self, size, buckets, max_gradient_norm=5.0, 
-			batch_size=64, learning_rate=0.5, forward_only=False):
+	def __init__(self, size, buckets, dropout, max_gradient_norm=5.0, 
+			batch_size=64, learning_rate=0.5, forward_only=False, trainable=False):
+
+		self.learning_rate = tf.Variable(float(learning_rate), trainable=trainable)
+		self.global_step = tf.Variable(0, trainable=trainable, name='global_step')
 
 		softmax_loss_function = None
 		self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
 
 		print("Creating %d layers of %d units." % (FLAGS.depth, size))
-		if model_types is GRU:
+		if FLAGS.model_types is 'GRU':
 			single_cell = tf.nn.rnn_cell.GRUCell(size)
-		elif model_types is LSTM:
+		elif FLAGS.model_types is 'LSTM':
 			single_cell = tf.nn.rnn_cell.BasicLSTMCell(size)
 		else:
 			print("model types not found!")
 			sys.exit(1)
-			cell = single_cell
-			cell = tf.nn.rnn_cell.MultiRNNCell([single_cell])
+		if not forward_only and dropout < 1:
+			single_cell = tf.nn.rnn_cell.DropoutWrapper(
+				single_cell, output_keep_prob=dropout)
+		cell = single_cell
+		cell = tf.nn.rnn_cell.MultiRNNCell([single_cell])
 
 		# The seq2seq function: we use embedding for the input.
 		def seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
 			return tf.nn.seq2seq.embedding_rnn_seq2seq(
-					encoder_inputs, decoder_inputs, cell, vocab_size,
-					o_vocab_size, size, feed_previous=do_decode)
+					encoder_inputs, decoder_inputs, cell, FLAGS.vocab_size,
+					FLAGS.vocab_size, size, feed_previous=do_decode)
 
 		self.encoder_inputs = []
 		self.decoder_inputs = []
@@ -119,7 +111,7 @@ class rnnAutoEncoder(object):
 			# print (self.decoder_inputs)
 			self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(
 				self.encoder_inputs, self.decoder_inputs, targets,
-				self.target_weights, buckets, lambda x, y: seq2seq_f(x, y,False),
+				self.target_weights, buckets, lambda x, y: seq2seq_f(x, y, True),
 				softmax_loss_function=softmax_loss_function)
 		else:
 			self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(
@@ -128,7 +120,7 @@ class rnnAutoEncoder(object):
 				lambda x, y: seq2seq_f(x, y, False),
 				softmax_loss_function=softmax_loss_function)
 
-		params = tf.trainable_variable()
+		params = tf.trainable_variables()
 		if not forward_only:
 			self.gradient_norms = []
 			self.updates = []
@@ -143,10 +135,10 @@ class rnnAutoEncoder(object):
 
 	def layer_step(self, session, X, bucket_id, forward_only):
 		encoder_size, decoder_size = self.buckets[bucket_id]
-		if len(encoder_inputs) != encoder_size:
+		if len(X) != encoder_size:
 			raise ValueError("Encoder length must be equal to the one in bucket,"
 						" %d != %d." % (len(X), encoder_size))
-		if len(decoder_inputs) != decoder_size:
+		if len(X) != decoder_size:
 			raise ValueError("Decoder length must be equal to the one in bucket,"
 						" %d != %d." % (len(X), decoder_size))
 
@@ -276,11 +268,11 @@ def read_data(train, max_size=None):
 				break
 	return data_set
 """
-def create_model_softmax(session, forward_only):
+def create_model_softmax(session, forward_only, trainable):
 	model = rnnAutoEncoder(
-		FLAGS.embedding_size, _buckets, FLAGS.max_gradient_norm,
-		FLAGS.batch_size, FLAGS.learning_rate, forward_only=forward_only)
-	"""
+		FLAGS.embedding_size, unsup_buckets, FLAGS.dropout, FLAGS.max_gradient_norm,
+		FLAGS.batch_size, FLAGS.learning_rate, forward_only=forward_only, trainable=trainable)
+	
 	ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
 	if ckpt and tf.gfile.Exists(ckpt.model_checkpoint_path):
 		print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
@@ -288,14 +280,14 @@ def create_model_softmax(session, forward_only):
 	else:
 		print("Created model with fresh parameters.")
 		session.run(tf.initialize_all_variables())
-	"""
+	
 	return model
 
 def main(_):
 	""" train a sentiment analysis mode using """
 	print ("[*] Preparing IMDB sentiment data... in %s" % FLAGS.data_dir)
 	#load_data, prepare_data = get_dataset('imdb_unsup')
-	
+	sentiment_data = TextLoader(FLAGS.data_dir, FLAGS.batch_size, FLAGS.vocab_size)
 
 	with tf.Graph().as_default(), tf.Session() as sess:
 
@@ -303,51 +295,64 @@ def main(_):
 		print('Loading unsupervise training data')
 		#train, valid = load_data(n_words=FLAGS.vocab_size, valid_portion=0.05, maxlen=FLAGS.max_len)
 		#train_set = read_data()
-		train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
-		train_total_size = float(sum(train_bucket_sizes))
-		train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size
-								for i in xrange(len(train_bucket_sizes))]
 
-		print('create the model')
-		model = create_model_softmax(sess, FLAGS.embedding_size, forward_only=False)
-
+		#train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+		#train_total_size = float(sum(train_bucket_sizes))
+		#train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size
+		#						for i in xrange(len(train_bucket_sizes))]
+		
 		step_time, loss = 0.0, 0.0
 		current_step = 0
 		previous_losses = []
+		model_l = []
 
 		# unsupervised layer-wise train
 		for i in range(FLAGS.depth):
-			for j in range(FLGAS.max_epoch):
-				random_number_01 = np.random.random_sample()
-				bucket_id = min([i for i in xrange(len(train_buckets_scale))
-                		if train_buckets_scale[i] > random_number_01])
+			print('create the model in layer ', i)
+			model_l.append(create_model_softmax(sess, forward_only=False, trainable=True))
+
+			print("start unsupervised pre-train")
+			while True:
+				_encoder_inputs = []
+				encoder_inputs, _, bucket_id = sentiment_data.next_unsup()
+				# TODO: change bucket id
+				_bucket_id = bucket_id
+				_encoder_inputs.append(encoder_inputs)
+				if i > 0:
+					for k in range(i):
+						_, step_loss, _encoder_inputs_tmp= model_l[k].layer_step(sess, _encoder_inputs[k], _bucket_id, True)
+						_encoder_inputs.append(_encoder_inputs_tmp)
 
 				start_time = time.time()
 				# load each batch
-				encoder_inputs, bucket_id = model.get_batch(train, bucket_id)
 
-				_, step_loss, _ = model.layer_step(sess, encoder_inputs, bucket_id, False)
+				_, step_loss, _ = model_l[i].layer_step(sess, _encoder_inputs[i], _bucket_id, False)
 				step_time += (time.time() - start_time) / FLAGS.steps_per_checkpoint
 				loss += step_loss / FLAGS.steps_per_checkpoint
 				current_step += 1
 
+				print(" validation ...")
 				if current_step % FLAGS.steps_per_checkpoint == 0:
 					perplexity = math.exp(loss) if loss < 300 else float('inf')
 					print ("global step %d learning rate %.4f step-time %.2f perplexity "
-				    		"%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
-							step_time, perplexity))
+						"%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
+						step_time, perplexity))
 					previous_losses.append(loss)
 					# Save checkpoint and zero timer and loss.
 					checkpoint_path = os.path.join(FLAGS.train_dir, "sentiment.ckpt")
 					model.saver.save(sess, checkpoint_path, global_step=model.global_step)
 					step_time, loss = 0.0, 0.0
 					# Run evals on development set and print their perplexity.
-					for bucket_id in xrange(len(_buckets)):
-						encoder_inputs, decoder_inputs, bucket_id = sentiment_data.next_valid()
-						_, eval_loss, _ = model.step(sess, encoder_inputs, decoder_inputs, bucket_id, True)
+					for bucket_id in xrange(len(unsup_buckets)):
+						encoder_inputs, decoder_inputs, bucket_id = sentiment_data.next_unsup_valid()
+						_, eval_loss, _ = model_l[i].layer_step(sess, _encoder_inputs[i], _bucket_id, True)
 						eval_ppx = math.exp(eval_loss) if eval_loss < 300 else float('inf')
-						print("  eval: bucket %d perplexity %.2f" % (bucket_id, eval_ppx))
+						print("  eval: bucket %d perplexity %.2f" % (_bucket_id, eval_ppx))
 					sys.stdout.flush()
+				if (loss < 0.1) or (current_step < max_step):
+					print("unsupervised training done, global step %d, perplexity: %.2f, learning rate %.4f"
+						% (model.global_step.eval(), perplexity, model.learning_rate.eval()))
+					break
 
 
 
